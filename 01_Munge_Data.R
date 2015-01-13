eeb073a81d80b87b742e515f41089aae55cd9b36
skparkes <bias.variance@gmail.com>
2015-01-12 21:19:35 -0500

    Enable route display.
    Do this to better understand data capture characteristics.

--- 01_Munge_Data.R
+++ 01_Munge_Data.R
@@ -2,40 +2,65 @@
 library(dplyr)
 library(magrittr)
 library(ggmap)
+library(scales)
+library(RColorBrewer)
 
 print(dir.data <- paste0(Sys.getenv('UserProfile'),'/Google Drive/Skunkworks_OnePlus/'))
 print(commute.src <- src_sqlite(paste0(dir.data,'commute.sqlite')))
-glimpse(tbl.location <- tbl(commute.src, 'location'))
-print(i.bbox <- make_bbox(long, lat, tbl.location %>% collect(), f=0.2))
-print(i.bbox <- i.bbox * c(0.999, 1, 1.001, 1))
 
+print(tbl.commute <- tbl(commute.src, sql("
+  SELECT src.*,ref.duration_min
+  FROM location as src
+  INNER JOIN commute as ref on
+    src.date = ref.date
+    and src.time between (ref.time - ref.duration_min/60 - 1) and (ref.time + 1)
+  ORDER BY src.date, src.time desc
+")) %>% collect())
+
+print(i.bbox <- make_bbox(long, lat, tbl.commute, f=0.2))
 i.osm <- get_openstreetmap(bbox = i.bbox, scale=1E5, messaging=TRUE)
-ggmap(i.osm) + 
-  geom_point(
-    data=tbl.location %>%
-      collect()
-    ,aes(x=long,y=lat)
-    ,size=9
-    ,color='red'
-    )
 
-i.google <- get_googlemap(
-  tbl.location %>% collect() %>% summarize(lon=mean(range(long)),lat=mean(range(lat))) %$% c(lon,lat)
-  ,zoom=11)
-ggmap(i.google) + 
-  geom_point(
-    data=tbl.location %>%
-      collect() %>%
-      mutate(
-        fuzzy_acc = factor(ifelse(accuracy<420,'Good','Bad'))
-        ,direction = factor(ifelse(time>12,'Evening','Morning'))
-        ,date_direction = factor(paste0(date,as.character(direction)))
-        )
+glimpse(tbl.prep <- tbl.commute %>%
+  mutate(
+    fuzzy_acc = factor(ifelse(accuracy<420,'Good','Bad'))
+    ,direction = factor(ifelse(time>12,'Evening','Morning'))
+    ,date_direction = factor(paste0(date,as.character(direction)))
+  ))
+
+
+plt.commute <- ggmap(
+  i.osm
+  ,base_layer=ggplot(
+    data=tbl.prep
     ,aes(
       x=long
       ,y=lat
-      ,color=date_direction
+      ,color=duration_min
       )
+    )
+  ) + 
+  geom_point(
+    aes(alpha=1/sqrt(accuracy))
     ,size=5
-    #,color='orange'
-  ) + scale_color_brewer(palette = 'Dark2')
+    ) +
+  geom_path(
+    aes(group=date_direction)
+    ,size=1
+    ,alpha=0.8
+    ) +
+  scale_size_continuous()+
+  scale_alpha_continuous(
+    guide='none'
+    ,range=c(0.5,0.95)
+    )+
+  scale_color_gradientn(
+    "Duration\n(Minutes)"
+    ,colours=rev(brewer.pal(11, 'RdYlBu'))[-6]
+    )
+plt.commute
+ggsave(
+  paste0(dir.data,'commute.png')
+  ,plt.commute
+  ,width=8
+  ,height=8
+  )
