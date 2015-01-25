1e255ddf34761ee98653c49d98744b359280d8a8
skparkes <bias.variance@gmail.com>
2015-01-24 22:55:46 -0500

    Cleanup and comment heatmap code.
    Do this so I can follow my work later.

--- 01_Munge_Data.R
+++ 01_Munge_Data.R
@@ -81,24 +81,31 @@ ggsave(
   )
 
 ## Approximate miles conversions from here: http://geography.about.com/library/faq/blqzdistancedegree.htm
+## Some useful Raster info here: http://stackoverflow.com/questions/25847188/geographical-heat-map-of-a-custom-property-in-r-with-ggmap
 
+#' Make the grid of points to measure
 n.tiles.wide <- 420L
 i.tiles <- expand.grid(
   lon = seq(i.bbox['left'], i.bbox['right'], length.out = n.tiles.wide)
   ,lat = seq(i.bbox['bottom'], i.bbox['top'], length.out = n.tiles.wide)
   )
+
+#' Calculate distances between measurement points and observed points
+threshold.miles <- 2.5 ## Distances above this do not get captured
 i.dist <- nearest.dist(
-  x=as.matrix(tbl.prep %>% select(long, lat))
-  ,y=as.matrix(i.tiles)
+  x=as.matrix(tbl.prep %>% select(long, lat)) ## Observed
+  ,y=as.matrix(i.tiles) ## Measured
   ,method='greatcircle'
-  ,delta = 2.5 * (360/(3963.34*2*pi)) ##Converts from miles to necessary ~delta
+  ,delta = threshold.miles * (360/(3963.34*2*pi)) ##Converts from miles to necessary ~delta
   )
-str(i.dist)
+hist(i.dist@entries, breaks='FD')
+
+#' Calculate weights from distances
 i.dist.wgt <- i.dist
-hist(i.dist.wgt@entries, breaks='FD')
 i.dist.wgt@entries <- 1/(exp(i.dist.wgt@entries^2))
 hist(i.dist.wgt@entries, breaks='FD')
 
+#' Bring back information to the measurement points
 i.tiles$denom <- colSums(i.dist.wgt)
 i.tiles$num <- as.vector(t(i.dist.wgt) %*% matrix(tbl.prep$duration_min, ncol = 1))
 hist(i.tiles$denom, breaks=42)
@@ -119,39 +126,34 @@ i.tiles %<>% mutate(
       ,(duration_min_avg.cap - min(duration_min_avg.cap, na.rm=TRUE))
         / (max(duration_min_avg.cap, na.rm=TRUE) - min(duration_min_avg.cap, na.rm=TRUE))
       )
-    #,color = MyRampWrap(duration_min_avg.scale, alpha.scale)
     )
 hist(i.tiles$alpha.scale, breaks=42)
 hist(i.tiles$duration_min_avg.scale, breaks=42)
 summary(i.tiles)
 
-
+#' Calculate an interesting color to help build a raster object
 MyRamp <- colorRamp(brewer.pal(11, 'RdBu')[-(5:7)])
-# seq.sample <- seq(0,1,by = 0.01)
-# seq.sample.color.ramp <- MyRamp(seq.sample)
-# seq.sample.color.vector <- rgb(seq.sample.color.ramp[,1],seq.sample.color.ramp[,2],seq.sample.color.ramp[,3],alpha=255,maxColorValue=255)
-# plot(x=seq.sample,y=rep(1,length(seq.sample)),col=seq.sample.color.vector,pch=16)
 MyRampWrap <- function(i.color.norm, i.alpha.norm=1, i.ramp.func = MyRamp) {
   i.color.ramp <- i.ramp.func(i.color.norm)
   rgb(i.color.ramp[,1], i.color.ramp[,2], i.color.ramp[,3], i.alpha.norm*255, maxColorValue = 255L)
-}
+  }
+
 i.tiles$raster.color <- rgb(0,0,0,0)
-i.tiles$raster.color[!is.na(i.tiles$duration_min_avg.scale)] <- MyRampWrap(
-  i.tiles$duration_min_avg.scale[!is.na(i.tiles$duration_min_avg.scale)]
-  ,sqrt(i.tiles$alpha.scale[!is.na(i.tiles$duration_min_avg.scale)])
+ind.empty <- is.na(i.tiles$duration_min_avg.scale) %T>% {mean(.) %>% print()}
+i.tiles$raster.color[!ind.empty] <- MyRampWrap(
+  i.tiles$duration_min_avg.scale[!ind.empty]
+  ,sqrt(i.tiles$alpha.scale[!ind.empty])
   )
-i.tiles$raster.color %>% table() %>% sort() %>% tail(20)
+my.raster <- matrix(i.tiles$raster.color, nrow=n.tiles.wide, byrow=TRUE)
 
-my.raster <- matrix(i.tiles$raster.color, nrow=n.tiles.wide,byrow=TRUE)
-str(my.raster)
-
-## Some useful Raster info here: http://stackoverflow.com/questions/25847188/geographical-heat-map-of-a-custom-property-in-r-with-ggmap
-
+#' Plot the resulting heat map
 plt.heat <- ggmap(
   i.osm
   ,darken = c(0.3, 'white')
-) + 
-  inset_raster(my.raster, xmin = i.bbox['left'], xmax=i.bbox['right'], ymin=i.bbox['bottom'],ymax=i.bbox['top'])
-  
+  ) + 
+  inset_raster(
+    my.raster
+    ,xmin = i.bbox['left'], xmax=i.bbox['right']
+    ,ymin=i.bbox['bottom'],ymax=i.bbox['top']
+    )
 plt.heat
-
