f35c9a5a9412c995b9014832377c31faf7a4ab6f
skparkes <bias.variance@gmail.com>
2015-01-27 21:31:34 -0500

    Move commute.sqlite local if we can reach it.
    Do this in anticipation of pushing our app to the cloud.

--- app-commute/server.R
+++ app-commute/server.R
@@ -1,8 +1,12 @@
 library(shiny)
 source('logic.R')
 
+#' Snag a fresh commute database if we can reach it
 dir.data <- paste0(Sys.getenv('UserProfile'),'/Google Drive/Skunkworks_OnePlus/') %T>% print()
-commute.src <- src_sqlite(paste0(dir.data,'commute.sqlite')) %T>% print()
+filepath.commute.db <- paste0(dir.data,'commute.sqlite') %>% print()
+if(file.exists(filepath.commute.db)) {file.copy(filepath.commute.db, getwd())}
+
+commute.src <- src_sqlite('commute.sqlite') %T>% print()
 
 i.components <- GenerateComponents(commute.src)
 
