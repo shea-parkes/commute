a25165cd8d1317966417c9bbf5031f3c0095b3b1
skparkes <bias.variance@gmail.com>
2015-01-29 20:56:14 -0500

    Add print statements to server.R.
    Do this so it is easier to diagnose errors in the cloud when only the
    printed log is available.

--- app-commute/server.R
+++ app-commute/server.R
@@ -1,15 +1,23 @@
 library(shiny)
-source('logic.R')
+print('Initializing Shiny App')
 
+print('Loading logic file')
+source('logic.R')
+print('Finished loading logic file')
+
+print('Attempting to grab fresh database')
 #' Snag a fresh commute database if we can reach it
 dir.data <- paste0(Sys.getenv('UserProfile'),'/Google Drive/Skunkworks_OnePlus/') %T>% print()
 filepath.commute.db <- paste0(dir.data,'commute.sqlite') %>% print()
 if(file.exists(filepath.commute.db)) {file.copy(filepath.commute.db, getwd())}
+print('Finished attempting to grab fresh database')
 
+print('Generating base objects')
 commute.src <- src_sqlite('commute.sqlite') %T>% print()
+i.components <- GenerateComponents(commute.src) %T>% {str(.) %>% print()}
+print('Finished generating base objects')
 
-i.components <- GenerateComponents(commute.src)
-
+print('Moving into reactive state')
 shinyServer(function(input, output, session) {
   output$heatmap <- renderPlot(
     CreateHeatMap(
